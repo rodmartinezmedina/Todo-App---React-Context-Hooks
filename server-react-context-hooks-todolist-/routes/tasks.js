d646473f9ee3f3120b7663fdd992cf8b7f24c660
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-05 01:11:08 +0200

    start tasks router"

---
+++ server-react-context-hooks-todolist-/routes/tasks.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user-model");
+const Task = require("../models/task-model");
+
+// GET 1 TASK
+
+
+
+// CREATE TASK
+
+
+
+// UPDATE Task
+
+
+
+// DELETE Task
