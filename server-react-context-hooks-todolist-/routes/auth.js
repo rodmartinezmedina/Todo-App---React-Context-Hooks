26e1380141eaed5ea6bb8b2eb8bbfd191005923b
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-04 23:19:27 +0200

    start auth route

---
+++ server-react-context-hooks-todolist-/routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const createError = require("http-errors");
+const bcrypt = require("bcrypt");
+const User = require("../models/user-model");
+const saltRounds = 10;
+
+// get User
+
+
+
+// create User
+
+
+
+// login User
+
+
+
+// logout User
--- server-react-context-hooks-todolist-/routes/users.js
+++ server-react-context-hooks-todolist-/routes/users.js
@@ -1,9 +1,12 @@
-erokuapp.com/var express = require('express');
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.json([{
+    
+  }])
 });
 
 module.exports = router;
